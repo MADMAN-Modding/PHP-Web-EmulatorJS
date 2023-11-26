3a00e0a993262ba3e59137c3ff78955829747881
MADMAN-Modding <mad@kmdtech.net>
2023-11-26 16:54:16 -0500

    PHP IS DONE, documentation to come

--- index.php
+++ index.php
@@ -1,7 +1,3 @@
-<?php
-    session_start();
-?>
-
 <!DOCTYPE html>
 <html>
     <head>
@@ -16,33 +12,58 @@
         <link type="text/css" rel="stylesheet" href="css/index.css"/>
     </head>
     <body>
-        <h1 id="title">Welcome to E.P.W.F.</h1>
+        <h1 id="title">Welcome to E.P.W.F.<a href="https://github.com/MADMAN-Modding/EPWF" target="_blank"><img class="github" src="https://github.com/favicon.ico"></a></h1>
 
         <h2>Or EmulatorJS PHP Web Frontend</h2>
         
+        <h3>Hover/Tap on a console to list the games</h3>
         <?php
-            echo "<div id='gb' class='gb'>";
-            echo "<h2 id='gb'>GameBoy Games</h2>";
-                if ($scanner = opendir('roms/gb')) {
-                    while (false !== ($entry = readdir($handle))) {
-                        if ($entry != "." && $entry != "..") {
-                            $files[] = $entry;
-                        }
-                    }
-                    closedir($scanner);
-
-                    natsort($files);
-                    foreach($files as $file) {
-                        echo "<a id='gb-link' href='game.html?rom=$file&console=gb'>";
-                        // Replace .gb .gbc .gba .7z .rar and .zip with nothing
-                            $file = str_replace(".gb","",$file);
-                            $file = str_replace(".7z","",$file);
-                            $file = str_replace(".rar","",$file);
-                            $file = str_replace(".zip","",$file);
-                        echo "$file</a>";
+            if ($consoleFinder = opendir('roms/')) {
+                $consoleList = [];
+                $files = [];
+                while (false !== ($console = readdir($consoleFinder))) {
+                    if ($console != "." && $console != "..") {
+                        $consoleList[] = $console;
                     }
                 }
-                echo "</div>";
+                // Closes the directory to save resources
+                closedir($consoleFinder);
+
+                // Alphabetically sorts the consoles
+                natsort($consoleList);
+                foreach($consoleList as $consoleName) {              
+                    
+                    // Scans the rom folders
+                    if ($scanner = opendir("roms/$consoleName")) {
+                        // Creates the Div
+                            echo "<div class=$consoleName>";
+                        // Makes the Title
+                            echo "<h2 class=$consoleName>", strtoupper($consoleName), " Games</h2>";
+                            
+                            $files = []; // Reset $files array for each console
+                        while (false !== ($entry = readdir($scanner))) {
+                            if ($entry != "." && $entry != "..") {
+                                $files[] = $entry;
+                            }
+                        }
+                        natsort($files);
+                        foreach($files as $file) {
+                            
+                            // Replace the console extension .7z .rar and .zip with nothing
+                                $file = str_replace(".$consoleName","",$file);
+                                $file = str_replace(".sfc","",$file);
+                                $file = str_replace(".7z","",$file);
+                                $file = str_replace(".rar","",$file);
+                                $file = str_replace(".zip","",$file);
+                            echo "<a class=$consoleName-link href=game.html?rom=$file&console=$consoleName>";
+
+                            echo "$file</a>";
+                        }
+                        echo "</div>";
+                    }
+                }
+            }
+                
         ?>
     </body>
 </html>
