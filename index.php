65b8cdfeda87c63acd1254e62d58a40356e9a6a8
MADMAN-Modding <115199645+MADMAN-Modding@users.noreply.github.com>
2023-11-27 07:14:57 -0500

    Update index.php

--- index.php
+++ index.php
@@ -12,9 +12,9 @@
         <link type="text/css" rel="stylesheet" href="css/universal.css"/>
     </head>
     <body>
-        <h1 id="title">Welcome to E.P.W.B.<a href="https://github.com/MADMAN-Modding/EPWF" target="_blank"><img class="github" src="https://github.com/favicon.ico"></a></h1>
+        <h1 id="title">Welcome to E.P.W.F.<a href="https://github.com/MADMAN-Modding/EPWF" target="_blank"><img class="github" src="https://github.com/favicon.ico"></a></h1>
 
-        <h2>Or EmulatorJS PHP Web Backend</h2>
+        <h2>Or EmulatorJS PHP Web Frontend</h2>
         
         <h3>Hover/Tap on a console to list the games</h3>
 
